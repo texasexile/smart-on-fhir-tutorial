d837caa3bec1fbab6d8526e6cc9c226cd2ab6bc3
Ted Conley <ted.conley@gmail.com>
2019-07-01 15:51:44 -0400

    Added suppO2

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -17,7 +17,8 @@
                       code: {
                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4',
+                             'http://loinc.org|88658-0']
                       }
                     }
                   });
@@ -41,6 +42,7 @@
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
+          var suppO2 = byCodes('88658-0');
 
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
@@ -83,6 +85,7 @@
       diastolicbp: {value: ''},
       ldl: {value: ''},
       hdl: {value: ''},
+      suppO2: {value: ''},
     };
   }
 
@@ -126,6 +129,7 @@
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
     $('#hdl').html(p.hdl);
+    $('#suppO2').html(p.suppO2);
   };
 
 })(window);
